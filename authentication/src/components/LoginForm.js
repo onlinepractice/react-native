b4354de943dcd6ff3fd1724763720c98b6a57404
pshgpt <32108574+pshgpt@users.noreply.github.com>
2017-09-21 15:37:49 +0000

    Update LoginForm.js

--- authentication/src/components/LoginForm.js
+++ authentication/src/components/LoginForm.js
@@ -1,24 +1,50 @@
 import React, { Component } from 'react';
+import { Text } from 'react-native';
+
 import { Card, Button, CardSection, Input } from './common';
 
-
 class LoginForm extends Component {
-  state = { text: '' };
+  state = { email: '' };
+  state = { password: '' };
   render() {
 return (
       <Card>
-         <CardSection>
-           <Input>
-           label="E-mail"
-             value={this.state.text}
-             onChangeText={text => this.setState({ text })}
-           </Input>
+         <CardSection >
+           <Text style={{ fontSize: 20 }}>
+             E-mail
+           </Text>
          </CardSection>
-         <CardSection / >
+
          <CardSection>
-         <Button>
-         Login
-         </Button>
+           <Input
+             keyboardType='email-address'
+             placeholder="user@gmail.com"
+             value={this.state.email}
+             onChangeText={email => this.setState({ email })}
+           / >
+         </CardSection>
+
+
+         <CardSection >
+           <Text style={{ fontSize: 20 }}>
+             Password
+           </Text>
+         </CardSection>
+
+         <CardSection>
+           <Input
+             secureTextEntry
+             placeholder="password"
+             value={this.state.password}
+             onChangeText={password => this.setState({ password })}
+           / >
+         </CardSection>
+
+
+         <CardSection>
+           <Button>
+             Login
+           </Button>
          </CardSection>
 
       </Card>
