2f0aa4fa042c2bd716d191db7ec9c199a7bb19cf
pshgpt <32108574+pshgpt@users.noreply.github.com>
2017-09-21 15:39:33 +0000

    Update Input.js

--- authentication/src/components/common/Input.js
+++ authentication/src/components/common/Input.js
@@ -1,40 +1,30 @@
 import React from 'react';
-import { View, Text, TextInput } from 'react-native';
+import { TextInput } from 'react-native';
 
-const Input = ({ label, value, onChangeText, placeholder }) => {
-  const { inputStyle, labelStyle, containerStyle } = styles;
+const Input = ({ value, onChangeText, placeholder, secureTextEntry }) => {
+  const { inputStyle } = styles;
   return (
-    <View style={containerStyle}>
-      <Text style={labelStyle}>{label}</Text>
+
+
       <TextInput
+         secureTextEntry={secureTextEntry}
         placeholder={placeholder}
         autoCorrect={false}
         style={inputStyle}
         value={value}
         onChangeText={onChangeText}
       / >
-    </View>
+
  );
 };
 const styles = {
   inputStyle: {
     color: '#000',
-    paddingRight: 5,
-    paddingLeft: 5,
+    marginRight: 5,
+    marginLeft: 5,
     fontSize: 18,
     lineHeight: 23,
     flex: 2
-  },
-  labeStyle: {
-    fontSize: 30,
-    paddingLeft: 20,
-    flex: 1
-  },
-  containerStyles: {
-    height: 40,
-    flex: 1,
-    flexDirection: 'row',
-    align: 'center'
   }
 };
 
