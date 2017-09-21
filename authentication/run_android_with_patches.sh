cd964cdd6d6e9a7e780178dab8cc00892bc32032
Sanjeev Vaishya <svaishya@users.noreply.github.com>
2017-09-22 01:41:40 +1000

    Update run_android_with_patches.sh
    initial commit without validation

--- authentication/run_android_with_patches.sh
+++ authentication/run_android_with_patches.sh
@@ -1,2 +1,11 @@
+#!/bin/bash
+#call environment script once
+./environment.sh
+#create folder assets under android/src/main folder, if it does not exist
+if (~ find -f $PROJECT_ROOT/android/src/main/assets) then
+  mkdir assets
+fi
+#run the path for the issue unable to load script from assets index.android.bundle
 react-native bundle --platform android --dev false --entry-file index.android.js --bundle-output android/app/src/main/assets/index.android.bundle --assets-dest  android/app/src/main/res
+#run the command to start the react native app on android phone or emulator
 react-native run-android
