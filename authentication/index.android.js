f9871697a8aeb1865e56f80cd4ea90bfedd0fea6
Sanjeev Vaishya <svaishya@users.noreply.github.com>
2017-09-24 02:13:22 +1000

    Update index.android.js
    fixed the appRegistry with correct component 'authentication'.

--- authentication/index.android.js
+++ authentication/index.android.js
@@ -5,4 +5,4 @@ import { AppRegistry } from 'react-native';
 import App from './src/App';
 
 //render it to the device
-AppRegistry.registerComponent('albums', () => App);
+AppRegistry.registerComponent('authentication', () => App);
