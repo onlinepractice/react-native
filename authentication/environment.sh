5d2525a58b3b94a00ffb7a179b140ee68d8849c6
Sanjeev Vaishya <svaishya@users.noreply.github.com>
2017-09-25 20:40:38 +1000

    Update environment.sh
    Added git repository location, authentication project location and packager custom port as environment variables to be used in package.json. REACT_PACKAGER_PORT is also used in project.pbxproj file.

--- authentication/environment.sh
+++ authentication/environment.sh
@@ -2,3 +2,6 @@
 export ANDROID_HOME=/usr/local/Android/sdk
 export PATH=$PATH:"$ANDROID_HOME/platform-tools"
 export PATH=$PATH:"$ANDROID_HOME/tools"
+export REACT_REPO_ROOT=/Users/svaishya75/onlinepractice/react-native
+export REACT_NATIVE_AUTH_PROJECT_PATH=$REACT_REPO_ROOT/authentication
+export REACT_PACKAGER_PORT=8088
